22fe627ce14bc6229c19a93780b965798a1b7ba2
Sebastien Albert <s.albert@sae.edu>
2026-06-15 17:01:24 +0200

    Fixing red lights, no red light behaviour

--- Assets/Scripts/RedLight.cs
+++ Assets/Scripts/RedLight.cs
@@ -1,10 +1,16 @@
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.InputSystem;
+using UnityEngine.Serialization;
 
 public class RedLight : MonoBehaviour
 {
     private Camera _camera;
     private bool _isGreen = true;
+
+    public bool IsGreen => _isGreen;
+    public UnityEvent IsGreenEvent;
+    public UnityEvent IsRedEvent;
 
     void Start()
     {
@@ -23,12 +29,25 @@ public class RedLight : MonoBehaviour
         Ray ray = _camera.ScreenPointToRay(mousePos);
 
         if (Physics.Raycast(ray, out RaycastHit hit))
-            OnClicked();
+        {
+            if (hit.collider.gameObject == this.gameObject)
+            {
+                OnClicked();
+            }
+        }
     }
 
     private void OnClicked()
     {
         _isGreen = !_isGreen;
         Debug.Log($"{gameObject.name} clicked — IsGreen ? {_isGreen}");
+        if (_isGreen)
+        {
+            IsGreenEvent.Invoke();
+        }
+        else
+        {
+            IsRedEvent.Invoke();
+        }
     }
 }
